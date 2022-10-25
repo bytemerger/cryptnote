7cd6f94912a862ed5cfce78fcfa59ec763d6dfa9
franc <onyishif@gmail.com>
2022-10-25 14:46:18 +0100

    create notesService

--- src/notes/notes.module.ts
+++ src/notes/notes.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { NotesController } from './notes.controller';
 import { Note, NoteSchema } from './schema/note.schema';
+import { NotesService } from './notes.service';
 
 @Module({
   imports: [MongooseModule.forFeature([{ name: Note.name, schema: NoteSchema }])],
-  controllers: [NotesController]
+  controllers: [NotesController],
+  providers: [NotesService]
 })
 export class NotesModule {}
